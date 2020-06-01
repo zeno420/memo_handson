d08d8657d0d55a79fb2c766787dc7d2ba87cca41
Zeno Berkhan <zenoberkhan@rocketmail.com>
2020-06-01 09:20:26 +0200

    Part 4:
    Database handling:
    
    - show settings.py: database setup (SQLite is default, others like
    Postgres can be configured here)
    
    - INSTALLED_APPS names all apps (here: the default ones) having data
    models; database schema will be initialized by calling `manage.py
    migrate` for all apps in INSTALLED_APPS.
    
    $ python manage.py migrate
    
    Model creation:
    
    - edit handson/memo/models.py to create Memo and Attachment model

--- handson/memo/models.py
+++ handson/memo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Memo(models.Model):
+    title = models.CharField(max_length=200)
+    text  = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+class Attachment(models.Model):
+    memo = models.ForeignKey(Memo, on_delete=models.CASCADE)
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
