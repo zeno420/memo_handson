7b711f079dd08c488e5e79b9a62fa65cbd4d1154
Zeno Berkhan <zenoberkhan@rocketmail.com>
2020-06-01 10:21:39 +0200

    Part 6:
    - add placeholder views to Create/Read/Update/Delete a memo.
    
    -> memo/views.py
    
    - route URLs to each view
    
    -> memo/urls.py
    
    After this, the URLs can be typed into the browser for testing.

--- handson/memo/urls.py
+++ handson/memo/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create/', views.create_Memo, name='create_Memo'),
+    path('<int:memo_id>/', views.read_Memo, name='read_Memo'),
+    path('<int:memo_id>/update', views.update_Memo, name='update_Memo'),
+    path('<int:memo_id>/delete', views.delete_Memo, name='delete_Memo'),
+    path('<int:memo_id>/create_attachment', views.create_Attachment, name='create_Attachment'),
+    path('<int:memo_id>/delete_attachment/<int:attachment_id>', views.delete_Attachment, name='delete_Attachment'),
 ]
--- handson/memo/views.py
+++ handson/memo/views.py
@@ -5,4 +5,28 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return HttpResponse("All memos:")
+
+
+def create_Memo(request):
+    return HttpResponse("You are creating a memo.")
+
+
+def read_Memo(request, memo_id):
+    return HttpResponse("You are viewing memo %s." % memo_id)
+
+
+def update_Memo(request, memo_id):
+    return HttpResponse("You are updating memo %s." % memo_id)
+
+
+def delete_Memo(request, memo_id):
+    return HttpResponse("You are deleting memo %s." % memo_id)
+
+
+def create_Attachment(request, memo_id):
+    return HttpResponse("You are creating an attachment for memo %s." % memo_id)
+
+
+def delete_Attachment(request, memo_id, attachment_id):
+    return HttpResponse("You are deleting attachmend %s from memo %s." % (attachment_id, memo_id))
